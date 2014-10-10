34be9eb0052375f7a471aec22f6def16d1d9b651
Jack Thorp <jackthorp13@gmail.com>
2014-10-10 16:24:00 +0100

    Navbar does not collapse so join is always visible. Site is responsive but arrows for deck naviagtion do not show on screens other than desktop?

--- ethical_app/routes/index.js
+++ ethical_app/routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'EthicalBay' });
+});
+
+router.get('/join', function(req, res) {
+  res.render('sign-up_form', {});
 });
 
 module.exports = router;
